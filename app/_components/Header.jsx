c6b05d6a6e5d5d10018ce667b1f515d79ce70284
KingAshu22 <ashishprasad9833@gmail.com>
2024-09-12 15:42:02 +0530

    updated header to show profile pic

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -10,11 +10,46 @@ import {
 } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
 import useAuth from "@/lib/hook";
+import axios from "axios";
+import Image from "next/image";
 
 const Header = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [profilePic, setProfilePic] = useState("");
+  const [linkId, setLinkId] = useState("");
   const isAuthenticated = useAuth();
   const router = useRouter();
+
+  useEffect(() => {
+    const storedNumber = localStorage?.getItem("mobile");
+    console.log("Stored Number:", storedNumber);
+
+    if (storedNumber !== undefined) {
+      setMobileNumber(JSON?.parse(storedNumber));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (mobileNumber) {
+      getArtist();
+    }
+  }, [mobileNumber]);
+
+  const getArtist = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.NEXT_PUBLIC_API}/artist/contact/+${mobileNumber}`
+      );
+
+      if (response.data) {
+        setProfilePic(response.data.profilePic);
+        setLinkId(response.data.linkid);
+      }
+    } catch (error) {
+      console.error("Error fetching artist:", error);
+    }
+  };
 
   useEffect(() => {
     setIsMounted(true);
@@ -69,23 +104,37 @@ const Header = () => {
               </ul>
             </PopoverContent>
           </Popover>
-          <div>
+          <div className="flex items-center gap-4">
             {isAuthenticated && (
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button variant="ghost" className="p-0">
-                    <LogOut className="w-6 h-6" />
-                  </Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                  <div className="p-4">
-                    <p>Are you sure you want to sign out?</p>
-                    <Button onClick={handleSignOut} className="mt-2">
-                      Sign Out
+              <>
+                <Link
+                  href={`/${linkId}/basic-details`}
+                  className="relative w-10 h-10 md:w-12 md:h-12 rounded-full overflow-hidden"
+                >
+                  <Image
+                    src={profilePic}
+                    alt="Profile Picture"
+                    layout="fill"
+                    objectFit="cover"
+                    className="rounded-full"
+                  />
+                </Link>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button variant="ghost" className="p-0">
+                      <LogOut className="w-6 h-6" />
                     </Button>
-                  </div>
-                </PopoverContent>
-              </Popover>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <div className="p-4">
+                      <p>Are you sure you want to sign out?</p>
+                      <Button onClick={handleSignOut} className="mt-2">
+                        Sign Out
+                      </Button>
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              </>
             )}
           </div>
         </div>
